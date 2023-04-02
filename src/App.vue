e8511fe29f316b4db49ad07e1ff1ebbb75b8faeb
Junita <junita.berglin@gmail.com>
2023-04-02 13:20:10 +0200

    🎉 first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Todo } from './models/Todo';
+
+const state = ref<Todo>({name: "", done: false, level: 0})
+
+function handleClick(){
+  console.log("Klickat på knappen ==> " +state.value.name, state.value.level, state.value.done);
+}
+
+</script>
+
+<template>
+
+<input type="text" v-model="state.name" />
+<input type="checkbox" v-model="state.done" />
+<input type="number" v-model="state.level" />
+
+<button @click="handleClick">KLICK</button>
+
+<p>{{ state.name }} - {{ state.level }} - {{ state.done }}</p> 
+
+</template>
+
+<style scoped>
+
+</style>
